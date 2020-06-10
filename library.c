de1df5b836e995311780feb38803897488ba8ddc
Nina <singlan.nina@gmail.com>
2020-06-10 17:58:13 +0200

    using fectch

--- library.c
+++ library.c
@@ -1,10 +1,9 @@
 #include "NuSMV-2.6.0/NuSMV/code/nusmv/core/dd/dd.h"
 #include "NuSMV-2.6.0/NuSMV/code/nusmv/core/dd/DDMgr.h"
 #include "NuSMV-2.6.0/NuSMV/code/nusmv/core/utils/array.h"
+#include "NuSMV-2.6.0/NuSMV/code/nusmv/core/utils/defs.h"
 
 #include <stdio.h>
-
-
 
 int computeAndWritePrimes(DDMgr_ptr dd, bdd_ptr b, const char* pathToTheFile){
     array_t * primes = bdd_compute_primes(dd, b);
@@ -12,7 +11,9 @@ int computeAndWritePrimes(DDMgr_ptr dd, bdd_ptr b, const char* pathToTheFile){
     FILE* file = fopen(pathToTheFile, "w");
 
     if(file != NULL){
-        fprintf(file, primes->space);
+        for(int i = 0; i < primes->num; i++) {
+            fprintf(file, array_fetch(dd_ptr, primes, i));
+        }
 
         fclose(file);
     }else{
